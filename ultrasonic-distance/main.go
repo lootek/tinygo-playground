057bf0c33a56f7affc1c25f8b3fc9af6f5342e2b
Piotr Lucinski <lootek87@gmail.com>
2020-01-19 22:53:09 +0100

    trying to get this working...

--- ultrasonic-distance/main.go
+++ ultrasonic-distance/main.go
@@ -17,30 +17,52 @@ func main() {
 	echo := machine.D3
 	echo.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
+	out := machine.D2
+	out.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	time.Sleep(time.Second * 5)
+	println("start")
+
 	for {
+		trigger.Configure(machine.PinConfig{Mode: machine.PinInput})
 		trigger.High()
 		time.Sleep(time.Microsecond * 10)
 		trigger.Low()
+		trigger.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-		eDur := 0
 		for {
-			if !echo.Get() {
-				if eDur == 0 {
-					println("c")
-					continue
-				}
-
-				println("b")
-				break
-			}
-
-			println("++")
-			eDur++
 			time.Sleep(time.Microsecond)
+
+			if out.Get() {
+				println("out")
+			}
+			if echo.Get() {
+				println("echo")
+			}
+			if trigger.Get() {
+				println("trigger")
+			}
 		}
 
-		//eDur = 5 * eDur / 58
-		println(eDur)
+		//eDur := 0
+		//for {
+		//	if !echo.Get() {
+		//		if eDur == 0 {
+		//			println("c")
+		//			continue
+		//		}
+		//
+		//		println("b")
+		//		break
+		//	}
+		//
+		//	println("++")
+		//	eDur++
+		//	time.Sleep(time.Microsecond)
+		//}
+		//
+		////eDur = 5 * eDur / 58
+		//println(eDur)
 
 		//for i := 0; i < eDur; i++ {
 		//	led.High()
